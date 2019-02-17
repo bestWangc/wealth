71d30ce11035921fabba58a091bd8650221919bd
wangc <wangchenphp@sina.com>
2019-02-17 09:05:10 +0800

    充值

--- application/index/controller/Recharge.php
+++ application/index/controller/Recharge.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 
 use think\Db;
 use think\facade\Request;
+use app\service\controller\Recharge as sRecharge;
 
 /**
  * 用户中心
@@ -21,5 +22,19 @@ class Recharge extends Base
             'list' => $list
         ]);
         return $this->fetch();
+    }
+
+    //充值
+    public function createRecharge(Request $request)
+    {
+        $rMoney = $request::post('recharge_money/d',0);
+        $rWay = $request::post('recharge_way/d');
+
+        if($rMoney < $this::$coinPrice){
+            return jsonRes(1,'最少充值{$this::$coinPrice}元');
+        }
+
+        $res = sRecharge::createRecharge($this::$uid,$rMoney,$rWay);
+        return $res;
     }
 }
