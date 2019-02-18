d0306544af24fb1a95e46cd236c2b0ff42901634
betterchen <wangchenphp@sina.com>
2019-02-18 21:25:55 +0800

    删除不需要代码

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -133,42 +133,4 @@ class User extends Base
             return jsonRes(1,'错误，请重试');
         }
     }
-
-    //抢购记录
-    public function buyLog(Request $request)
-    {
-        $choseUid = $request::param('choseUid/d',0);
-
-        $this->assign('choseUid',$choseUid);
-        return $this->fetch();
-    }
-
-    //抢购详细记录
-    public function buyLogDetails(Request $request)
-    {
-        $uid = $request::post('choseUid/d');
-        $where = ['so.user_id'=>$uid];
-        if(!$uid){
-            $where = ['su.parent_id'=>$this->uid];
-        }
-
-        $result = Db::name('order')
-            ->alias('so')
-            ->join('users su','su.id = so.user_id','left')
-            ->join('goods sg','sg.id = so.goods_id','left')
-            ->join('award_info sai','sai.id = so.award_id','left')
-            ->where($where)
-            ->field('so.id as order_id,su.`name`,sg.`name` as good_name,so.goods_num,so.amount,so.guessing,sai.term_num,so.created_date')
-            ->order('so.created_date desc')
-            ->select();
-
-        if(!empty($result)){
-            foreach ($result as $key => &$value){
-                $value['guessing'] = $value['guessing'] ? '丰年' : '瑞雪';
-                $value['created_date'] = date('Y-m-d H:i:s',$value['created_date']);
-            }
-        }
-        return jsonRes(0,'成功',$result);
-    }
-
 }
