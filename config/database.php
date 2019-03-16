a8d9aaf05704aa8ac7eb7852cfaecbc233050b94
betterchen <wangchenphp@sina.com>
2019-03-16 17:19:43 +0800

    database.php

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => Env::get('database.host','193.112.108.190'),
+    'hostname'        => Env::get('database.host','127.0.0.1'),
     // 数据库名
     'database'        => 'wasai',
     // 用户名
